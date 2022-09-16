82009a47d47246690060f35a14348433cee3ad08
luizpaulo78pqd <luizpaulo78pqd@gmail.com>
2022-09-16 11:45:05 -0300

    fix: Correct name for test

--- server/src/__tests__/queries/get-all-users.spec.ts
+++ server/src/__tests__/queries/get-all-users.spec.ts
@@ -1,6 +1,6 @@
 import { GET_ALL_USERS } from "../../schema/queries";
 
-describe("DGET ALL USERS QUERY", () => {
+describe("GET ALL USERS QUERY", () => {
   it("should return success for method", () => {
     expect(GET_ALL_USERS).not.toBe(undefined);
   });
